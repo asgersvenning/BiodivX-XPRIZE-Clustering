eae8dd4e8f32e8c9bf88bdf262d5ac41573a3f00
GuillaumeMougeot <guillaume.mougeot@laposte.net>
2024-06-28 17:49:54 +0200

    added the clustering workflow

---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# rm -rf /home/george/codes/BiodivX-XPRIZE-Clustering/test/data/20230730_tiny_fb/crops_clustered
+
+python /home/george/codes/BiodivX-XPRIZE-Clustering/cluster.py\
+ --in_folder /home/george/codes/BiodivX-XPRIZE-Clustering/test/data/20230730_tiny_fb/crops\
+ --meta_folder /home/george/codes/BiodivX-XPRIZE-Clustering/test/data/20230730_tiny_fb/metadata\
+ --device cuda:0
