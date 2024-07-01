b6545bdd07e3e137ec1a7e4be58486a4970b5556
GuillaumeMougeot <guillaume.mougeot@laposte.net>
2024-07-01 15:31:38 +0200

    removed test.sh

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# rm -rf /home/george/codes/BiodivX-XPRIZE-Clustering/test/data/20230730_tiny_fb/crops_clustered
-
-python /home/george/codes/BiodivX-XPRIZE-Clustering/cluster.py\
- --in_folder /home/george/codes/BiodivX-XPRIZE-Clustering/data/20230730_tiny_fb/crops\
- --meta_folder /home/george/codes/BiodivX-XPRIZE-Clustering/data/20230730_tiny_fb/metadata\
- --device cuda:0\
- --gs
