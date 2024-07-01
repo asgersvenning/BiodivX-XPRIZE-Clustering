42a8fe7efe87f9e46dc23f830f4555973e431aaa
GuillaumeMougeot <guillaume.mougeot@laposte.net>
2024-07-01 15:04:25 +0200

    tiny test

--- test.sh
+++ test.sh
@@ -4,4 +4,5 @@
 python /home/george/codes/BiodivX-XPRIZE-Clustering/cluster.py\
  --in_folder /home/george/codes/BiodivX-XPRIZE-Clustering/data/20230730_tiny_fb/crops\
  --meta_folder /home/george/codes/BiodivX-XPRIZE-Clustering/data/20230730_tiny_fb/metadata\
- --device cuda:0
+ --device cuda:0\
+ --gs
